f88068f9c06719a8570d644c5b9d38806483eb5f
MotolMarianne <motolmarianneee@gmail.com>
2022-05-30 04:53:48 +0800

    Initial files

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf=8">
+	<link rel="stylesheet" type="text/css" href="style.css"
+	<title> Header </title>
+</head>
+<body>
+		<header>
+			<div id="main">
+				<div class="logo"> </div>
+				<nav> 
+					<ul>
+						<li><a href="Home.html"class="active">Home</a></li>
+						<li><a href="About.html">About</a></li>
+						<li><a href="Discover.html">Discover</a></li>
+						<li><a href="faqs.html">Faqs</a></li>
+					</ul>
+				</nav>
+			</div>
+
+			<
+			<div class="text">
+				<p><?php echo "HI Folks, Welcome to:" ?> </p>
+				<h1> WATREAD </h1>
+				<span></span>
+				<p>We offer you BOOKS that</p>
+				<p> you'll desire! </p>
+				<br>
+				<input type="button" class="btn" value="READ NOW">
+				</div> 
+
+
+	   </header>
+
+</body>
+</html>
