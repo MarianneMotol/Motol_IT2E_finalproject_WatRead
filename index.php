08647662d37a12a5d4992651f1a8a8824e8958f4
MotolMarianne <motolmarianneee@gmail.com>
2022-05-31 02:11:07 +0800

    about files

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
+
 <!DOCTYPE html>
-<html>
+<html  lang="en">
 <head>
 	<meta charset="utf=8">
 	<link rel="stylesheet" type="text/css" href="style.css"
@@ -11,23 +12,25 @@
 				<div class="logo"> </div>
 				<nav> 
 					<ul>
-						<li><a href="Home.html"class="active">Home</a></li>
-						<li><a href="About.html">About</a></li>
-						<li><a href="Discover.html">Discover</a></li>
-						<li><a href="faqs.html">Faqs</a></li>
+						<li><a href="index.php"class="active">Home</a></li>
+						<li><a href="about/about.php">About</a></li>
+						<li><a href="discoveries/discoveries.php">Discover</a></li>
+						<li><a href="faqs/faqs.php">Faqs</a></li>
 					</ul>
 				</nav>
 			</div>
 
-			<
+			
 			<div class="text">
-				<p><?php echo "HI Folks, Welcome to:" ?> </p>
+				<p><?php echo "Hi Folks, Welcome to:" ?> </p>
 				<h1> WATREAD </h1>
 				<span></span>
 				<p>We offer you BOOKS that</p>
 				<p> you'll desire! </p>
 				<br>
-				<input type="button" class="btn" value="READ NOW">
+				<form action="discoveries/discoveries.php" method="POST">
+					<input type="submit" class="btn" value="READ NOW" />
+				</form>
 				</div> 
 
 
